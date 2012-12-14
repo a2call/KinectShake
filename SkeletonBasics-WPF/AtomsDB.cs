4152adbebcfb1e9d992bb0f7d8da0a9319908c35
salivian <salivian@gmail.com>
2012-12-14 12:17:10 -0500

    added new files

---
+++ SkeletonBasics-WPF/AtomsDB.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Drawing;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    class Atom
+    {
+        public enum AtomType { H, O };
+
+        float [] pos_ = new float[3];
+        AtomType t_ = AtomType.H;
+        public static Color [] atomcolors_ = { Color.White, Color.Red };
+        public static float [] atomradii_ = {0.3f,0.7f};
+
+
+        public Atom(AtomType t)
+        {
+            t_ = t;
+        }
+
+        public float x(){return pos_[0];}
+        public float y(){return pos_[1];}
+        public float z(){return pos_[2];}
+
+        public float r(){return atomradii_[(int)t_];}
+        public Color color() { return atomcolors_[(int)t_]; }
+
+
+        public void setPos(float x, float y, float z)
+        {
+            pos_[0] = x;
+            pos_[1] = y;
+            pos_[2] = z;
+        }
+               
+        public bool intersect(Atom a){
+            float dx = pos_[0] - a.pos_[0];
+            float dy = pos_[1] - a.pos_[1];
+            float dz = pos_[2] - a.pos_[2];
+
+            float d = (float) Math.Sqrt(dx * dx + dy * dy + dz * dz);
+
+            if (d > (a.r() + r()))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+    class AtomsDB
+    {
+        public ArrayList atomlist = new ArrayList();
+
+        public void addAtom(Atom atm)
+        {
+            atomlist.Add(atm);
+        }
+
+        public bool checkIntersection(Atom a)
+        {
+            foreach (Atom atm in atomlist)
+            {
+                if (a.intersect(atm))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ SkeletonBasics-WPF/Server.cs
@@ -0,0 +1,79 @@
+﻿//This code is adopted from http://msdn.microsoft.com/en-us/library/6y0e13d3.aspx
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+
+namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+    public static class Server
+    {
+        public static OpenGLWin oglwin_; //use this to pass commands to openglwindow 
+
+        // Incoming data from the client.
+        public static string data = null;
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.
+            // Dns.GetHostName returns the name of the 
+            // host running the application.
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[1];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8888);
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and 
+            // listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.
+                while (true)
+                {
+                    Debug.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.
+                    Debug.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+        }
+    }
+}
